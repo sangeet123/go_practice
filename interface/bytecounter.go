fc1a1af17c60e79490910b953b27677da33de6dc
sangeet <sangeet.dahal.contractor@hilton.com>
2018-08-22 14:33:57 -0500

    learning interface in go

---
+++ interface/bytecounter.go
@@ -0,0 +1,28 @@
+package main
+
+import(
+	"fmt"
+)
+
+type ByteCounter int
+
+// This function implements Writer interface with Write method
+func (c *ByteCounter) Write(p []byte) (int, error) {
+	*c += ByteCounter(len(p)) // conver int to ByteCounter
+	return len(p), nil
+}
+
+// Now ByteCounter can be passed to Fprintf since it implements Writer interface
+// func Fprintf(w io.Writer, format string, args ...interface{})
+
+func main() {
+	var c ByteCounter
+	c.Write([]byte("Hello"))
+	fmt.Println(c)
+
+	c = 0 // reset
+
+	var name = "Ryan"
+	fmt.Fprintf(&c, "Hello, %s", name) // Fprintf internally calls c.Write method so the length will b 11
+	fmt.Println(c)
+}
---
+++ interface/counterwriter.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+)
+
+func CountingWriter(w io.Writer) (io.Writer, *int64) {
+	r := int64(len(fmt.Sprintf("%s",w)))
+	return w, &r
+}
+
+func main() {
+	var b bytes.Buffer
+	b.Write([]byte("hello world"))
+	_, v := CountingWriter(&b)
+	fmt.Println(*v)
+}
